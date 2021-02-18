598aa1be086bd62bd01c4525d82ff08220565fb8
Javier Paez <javierlds@gmail.com>
2021-02-17 22:25:11 -0500

    Ajuste archivo comentarios

--- comentarios/comentarios.py
+++ comentarios/comentarios.py
@@ -2,7 +2,7 @@
 print("texto numero")
 print("texto numero dos")
 
-#comentario en python
+#comentario en python de multilinea
 """
 print("comentarios de multilinea")
 print("comentarios de multilinea")
